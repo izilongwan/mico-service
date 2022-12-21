57fa89ea6e7fadc31c50259fa8855f1a38444d0f
izilongwan <js.nb@qq.com>
2022-12-21 17:34:55 +0800

    feat: test 添加timecostAop

--- test/src/main/java/com/test/Application.java
+++ test/src/main/java/com/test/Application.java
@@ -4,14 +4,16 @@ import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@ComponentScan({ "com.test", "com.common.aop.TimecostAop" })
 @MapperScan({ "com.test.mapper", "com.test.dto" })
 @EnableFeignClients({ "com.feignapi.client" })
-public class TestApplication {
+public class Application {
 
 	public static void main(String[] args) {
-		SpringApplication.run(TestApplication.class, args);
+		SpringApplication.run(Application.class, args);
 	}
 
 }
