7c278d60febfed98ec2a1f4fd7f8ca3f1167213e
izilongwan <js.nb@qq.com>
2022-12-23 20:36:02 +0800

    feat: common 整合config常用配置文件

--- common/src/main/java/com/common/config/MybatisPlusConfig.java
+++ common/src/main/java/com/common/config/MybatisPlusConfig.java
@@ -1,13 +1,20 @@
 package com.common.config;
 
+import java.time.LocalDateTime;
+
+import org.apache.ibatis.reflection.MetaObject;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+import com.baomidou.mybatisplus.core.handlers.MetaObjectHandler;
 import com.baomidou.mybatisplus.extension.plugins.MybatisPlusInterceptor;
 import com.baomidou.mybatisplus.extension.plugins.inner.PaginationInnerInterceptor;
 
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j(topic = "cn")
 @Configuration
-public class MybatisPlusConfig {
+public class MybatisPlusConfig implements MetaObjectHandler {
     @Bean
     public MybatisPlusInterceptor mybatisPlusInterceptor() {
         MybatisPlusInterceptor mPlusInterceptor = new MybatisPlusInterceptor();
@@ -15,5 +22,18 @@ public class MybatisPlusConfig {
         mPlusInterceptor.addInnerInterceptor(new PaginationInnerInterceptor());
 
         return mPlusInterceptor;
+    }
+
+    @Override
+    public void insertFill(MetaObject metaObject) {
+        log.info("插入数据，更新时间 ===>");
+        this.setFieldValByName("createTime", LocalDateTime.now().toString(), metaObject);
+        this.setFieldValByName("updateTime", LocalDateTime.now().toString(), metaObject);
+    }
+
+    @Override
+    public void updateFill(MetaObject metaObject) {
+        log.info("更新数据，更新时间 ===>");
+        this.setFieldValByName("updateTime", LocalDateTime.now().toString(), metaObject);
     }
 }
