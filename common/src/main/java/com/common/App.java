a39674b7f5fa84111dde5503a5555196e077bbed
izilongwan <js.nb@qq.com>
2022-12-24 12:33:26 +0800

    feat: 优化common配置

--- common/src/main/java/com/common/App.java
+++ common/src/main/java/com/common/App.java
@@ -1,6 +1,5 @@
 package com.common;
 
-import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.servlet.ServletComponentScan;
@@ -8,7 +7,6 @@ import org.springframework.scheduling.annotation.EnableAsync;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 @SpringBootApplication
-@MapperScan({ "com.common.mapper" })
 @ServletComponentScan
 @EnableTransactionManagement
 @EnableAsync
