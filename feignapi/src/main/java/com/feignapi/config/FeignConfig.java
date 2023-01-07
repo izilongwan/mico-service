4a58a129d81a066f8f9795cc8599e4a69d1f974f
izilongwan <js.nb@qq.com>
2023-01-07 17:30:30 +0800

    fix: feign 转发添加请求头信息

---
+++ feignapi/src/main/java/com/feignapi/config/FeignConfig.java
@@ -0,0 +1,55 @@
+package com.feignapi.config;
+
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.Enumeration;
+import java.util.HashMap;
+import java.util.stream.Stream;
+
+import javax.servlet.http.HttpServletRequest;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.context.request.RequestContextHolder;
+import org.springframework.web.context.request.ServletRequestAttributes;
+
+import com.google.common.base.Objects;
+
+import feign.RequestInterceptor;
+import feign.RequestTemplate;
+
+@Configuration
+public class FeignConfig {
+    /**
+     * 转发请求头信息
+     *
+     * @return
+     */
+    @Bean
+    public RequestInterceptor requestInterceptor() {
+        return new RequestInterceptor() {
+
+            @Override
+            public void apply(RequestTemplate template) {
+                ServletRequestAttributes attrs = ((ServletRequestAttributes) RequestContextHolder
+                        .getRequestAttributes());
+
+                if (attrs != null) {
+                    HttpServletRequest request = attrs.getRequest();
+                    Enumeration<String> headerNames = request.getHeaderNames();
+
+                    if (headerNames != null) {
+                        while (headerNames.hasMoreElements()) {
+                            String key = headerNames.nextElement();
+
+                            if (!Objects.equal("host", key)) {
+                                String value = request.getHeader(key);
+                                template.header(key, value);
+                            }
+                        }
+                    }
+                }
+            }
+        };
+    }
+}
--- m3/src/main/java/com/m3/App.java
+++ m3/src/main/java/com/m3/App.java
@@ -4,9 +4,11 @@ import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
 @MapperScan({ "com.m3.mapper" })
+@ComponentScan(basePackages = { "com.m3", "com.feignapi.config" })
 // @EnableFeignClients(clients = { UserClient.class })
 @EnableFeignClients(basePackages = "com.feignapi.client")
 public class App {
