fac486995eed73d665b302926101ad6602993175
izilongwan <js.nb@qq.com>
2023-01-07 14:29:35 +0800

    fix: 修复点评webmvc WebMvcConfigurationSupport

--- dianping/src/main/java/com/dianping/config/WebMvccConfig.java
+++ dianping/src/main/java/com/dianping/config/WebMvccConfig.java
@@ -2,13 +2,13 @@ package com.dianping.config;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurationSupport;
 
 import com.dianping.interceptor.LoginInterceptor;
 import com.dianping.interceptor.RefreshInterceptor;
 
 @Configuration
-public class WebMvccConfig implements WebMvcConfigurer {
+public class WebMvccConfig extends WebMvcConfigurationSupport {
 
     @Override
     public void addInterceptors(InterceptorRegistry registry) {
