04a6950ae5df3291aa8c34b3d2f106b965a3e64d
izilongwan <js.nb@qq.com>
2023-01-16 21:10:11 +0800

    fix: exceptionhandle 减少暴露信息

--- common/src/main/java/com/common/advice/ExceptionHandlerAdvice.java
+++ common/src/main/java/com/common/advice/ExceptionHandlerAdvice.java
@@ -1,5 +1,6 @@
 package com.common.advice;
 
+import org.springframework.jdbc.BadSqlGrammarException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
@@ -7,6 +8,11 @@ import com.common.entity.R;
 
 @RestControllerAdvice
 public class ExceptionHandlerAdvice {
+    @ExceptionHandler
+    public R<Object> handler(BadSqlGrammarException e) {
+        return R.ERROR(e, e.getCause().toString());
+    }
+
     @ExceptionHandler
     public R<Object> handler(Exception e) {
         return R.ERROR(e, e.getMessage());
--- dianping/src/main/java/com/dianping/Application.java
+++ dianping/src/main/java/com/dianping/Application.java
@@ -7,12 +7,17 @@ import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.EnableAspectJAutoProxy;
 
+import com.common.advice.ExceptionHandlerAdvice;
 import com.common.config.MybatisPlusConfig;
 import com.common.config.RedisConfig;
 
 @SpringBootApplication
 @EnableAspectJAutoProxy(exposeProxy = true)
-@ComponentScan(basePackages = { "com.dianping" }, basePackageClasses = { MybatisPlusConfig.class, RedisConfig.class })
+@ComponentScan(basePackages = { "com.dianping" }, basePackageClasses = {
+		ExceptionHandlerAdvice.class,
+		MybatisPlusConfig.class,
+		RedisConfig.class
+})
 @MapperScan({ "com.dianping.mapper", "com.dianping.dto" })
 @EnableFeignClients({ "com.feignapi.client" })
 public class Application {
