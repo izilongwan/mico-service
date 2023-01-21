35392b50d8ccfc93bd96185621a8f781c2a07ad3
izilongwan <js.nb@qq.com>
2023-01-21 15:31:28 +0800

    feat: add ValidatorAop

---
+++ common/src/main/java/com/common/aop/anno/ValidatorAnno.java
@@ -0,0 +1,22 @@
+package com.common.aop.anno;
+
+import java.lang.annotation.Documented;
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+import org.springframework.core.annotation.AliasFor;
+
+@Documented
+@Target({ ElementType.PARAMETER, ElementType.FIELD, ElementType.METHOD })
+@Retention(RetentionPolicy.RUNTIME)
+public @interface ValidatorAnno {
+    @AliasFor("pattern")
+    String value() default "";
+
+    @AliasFor("value")
+    String pattern() default "";
+
+    String message() default "";
+}
