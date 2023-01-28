6c165c5e297d19fc6535ff219177c921a3fc1ede
izilongwan <js.nb@qq.com>
2023-01-28 21:33:32 +0800

    fix: ExceptionHandlerAdvice error

--- common/src/main/java/com/common/advice/ExceptionHandlerAdvice.java
+++ common/src/main/java/com/common/advice/ExceptionHandlerAdvice.java
@@ -10,12 +10,16 @@ import com.common.entity.R;
 public class ExceptionHandlerAdvice {
     @ExceptionHandler
     public R<Exception> handler(BadSqlGrammarException e) {
-        return Error(e);
+        return Error(e, true);
     }
 
     @ExceptionHandler
     public R<Exception> handler(Exception e) {
         return Error(e);
+    }
+
+    private R<Exception> Error(Exception e, boolean b) {
+        return R.ERROR(e, e.getCause().getMessage());
     }
 
     private R<Exception> Error(Exception e) {
