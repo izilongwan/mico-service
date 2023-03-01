4c043228230480a16835d19c2f67dbf31f8519b8
izilongwan <izilongwan@gmail.com>
2023-03-01 17:36:00 +0800

    feat: Dept add doc desc

--- test/src/main/java/com/test/entity/Dept.java
+++ test/src/main/java/com/test/entity/Dept.java
@@ -5,6 +5,8 @@ import java.io.Serializable;
 import com.baomidou.mybatisplus.annotation.FieldFill;
 import com.baomidou.mybatisplus.annotation.TableField;
 
+import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiModelProperty;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -12,9 +14,11 @@ import lombok.NoArgsConstructor;
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
+@ApiModel(description = "部门实体")
 public class Dept implements Serializable {
     public Integer id;
 
+    @ApiModelProperty(value = "部门名称")
     public String name;
 
     @TableField(fill = FieldFill.INSERT)
